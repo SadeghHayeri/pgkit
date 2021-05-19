14eda077c62b692bfbb839514178637b161b22c2
Nima Modares Gorji <nima.mg@gmail.com>
2021-05-19 14:54:09 +0430

    Add commands to cli

--- pgkit/cli/cli.py
+++ pgkit/cli/cli.py
@@ -3,7 +3,7 @@ from pgkit.cli.commands.config import config
 from pgkit.cli.commands.pitr import pitr
 from pgkit.cli.commands.status import status
 from pgkit.cli.commands.list import list
-from pgkit.cli.commands.shellx import shell
+from pgkit.cli.commands.shellx import shell, dumpall, dump, stop, restart, start
 
 
 @click.group()
@@ -17,4 +17,9 @@ def main():
     cli.add_command(status)
     cli.add_command(list)
     cli.add_command(shell)
+    cli.add_command(dump)
+    cli.add_command(dumpall)
+    cli.add_command(start)
+    cli.add_command(stop)
+    cli.add_command(restart)
     cli()
