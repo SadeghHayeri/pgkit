98680b2c49fbae882b6f684ee8590050454a7fa6
Parsa <parsa_ghorbani@outlook.com>
2021-12-18 12:38:49 +0330

    feat: Add one integration test

---
+++ deployment/integration-test.py
@@ -0,0 +1,32 @@
+import subprocess
+
+from pgkit.application.settings import DB_PATH
+
+
+EXPECTED_CONFIG = '{"config": {"1": {"name": "test", "version": "12", "host": "172.26.0.2", "port": 5432, "dbname": "test", "slot": "replica-test", "username": "test", "password": "test", "replica_port": "5432", "max_connections": 100, "max_worker_processes": 8\}\}\}'
+
+
+def add_config():
+    subprocess.run(
+        [
+            "pgkit",
+            "config",
+            "add",
+            "--name test",
+            "--version 12",
+            "--host 172.26.0.2",
+            "--port 5432",
+            "--dbname test",
+            "--slot replica-test",
+            "--username test",
+            "--password test",
+            "--replica-port 5432"
+        ],
+        capture_output=False
+    )
+
+    with open(DB_PATH, 'r') as f:
+        lines = f.readlines()
+        if lines[0] != EXPECTED_CONFIG:
+            exit(1)
+
