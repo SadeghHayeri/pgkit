f0995dde122fb1f88c00ad06b2bb22999edf19b6
Parsa <parsa_ghorbani@outlook.com>
2021-12-18 13:27:56 +0330

    fix: Fix test

--- deployment/integration-test.py
+++ deployment/integration-test.py
@@ -3,24 +3,15 @@ import subprocess
 from pgkit.application.settings import DB_PATH
 
 
-EXPECTED_CONFIG = '{"config": {"1": {"name": "test", "version": "12", "host": "172.26.0.2", "port": 5432, "dbname": "test", "slot": "replica-test", "username": "test", "password": "test", "replica_port": "5432", "max_connections": 100, "max_worker_processes": 8\}\}\}'
+EXPECTED_CONFIG = '{"config": {"1": {"name": "test", "version": "12", "host": "172.26.0.2", "port": 5432, "dbname": "test", "slot": "replica-test", "username": "test", "password": "test", "replica_port": "5432", "max_connections": 100, "max_worker_processes": 8}}}'
 
 
 def add_config():
     subprocess.run(
         [
-            "pgkit",
-            "config",
-            "add",
-            "--name test",
-            "--version 12",
-            "--host 172.26.0.2",
-            "--port 5432",
-            "--dbname test",
-            "--slot replica-test",
-            "--username test",
-            "--password test",
-            "--replica-port 5432"
+            "bash",
+            "-c",
+            "pgkit config add --name test --version 12 --host 172.26.0.2 --port 5432 --dbname test --slot replica-test --username test --password test --replica-port 5432"
         ],
         capture_output=False
     )
@@ -28,5 +19,11 @@ def add_config():
     with open(DB_PATH, 'r') as f:
         lines = f.readlines()
         if lines[0] != EXPECTED_CONFIG:
+            print("Config is not ok")
+            print(f"Real config {lines[0]}")
+            print(f"Expected config {EXPECTED_CONFIG}")
             exit(1)
+        else:
+            print("Config is good")
 
+add_config()
