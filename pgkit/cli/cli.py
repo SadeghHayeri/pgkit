9a4e25a2eba1326a18d08191a4b1d701f3ba0dd9
Nima Modares Gorji <nima.mg@gmail.com>
2022-06-06 11:01:50 +0430

    Fix: Remove status command from commands list

--- pgkit/cli/cli.py
+++ pgkit/cli/cli.py
@@ -1,7 +1,6 @@
 import click
 from pgkit.cli.commands.config import config
 from pgkit.cli.commands.pitr import pitr
-from pgkit.cli.commands.status import status
 from pgkit.cli.commands.shellx import shell, dumpall, dump, stop, restart, start, list
 
 
@@ -13,7 +12,6 @@ def cli():
 def main():
     cli.add_command(config)
     cli.add_command(pitr)
-    cli.add_command(status)
     cli.add_command(list)
     cli.add_command(shell)
     cli.add_command(dump)
